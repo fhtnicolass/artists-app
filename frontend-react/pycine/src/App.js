46aecc2538c91262c3cf27a9899a190a1086ab7b
Nicolas Farhat <nicolas.farhat@intelltech.com.br>
2022-12-14 09:30:55 -0300

    layout

--- frontend-react/pycine/src/App.js
+++ frontend-react/pycine/src/App.js
@@ -1,5 +1,6 @@
 
 import React, {Component} from 'react';
+import './App.css'
 
 var name = ''
 var id = ''
@@ -38,20 +39,33 @@ class App extends Component {
   
   render() {
       return(
-        <div className="App">
-            <button onClick={() => this.getArtistId()}>
-              Buscar Artista
-            </button>
-            
-            <input
-              type="text"
-              value={this.state.value}
-              onChange={this.handleChange}
-            />
-            
-            <div>{this.state.artist_name}</div>
-            <div>{this.state.biography}</div>
+        <div class="form-body">
+        <div class="row">
+            <div class="form-holder">
+                <div class="form-content">
+                    <div class="form-items">
+                        <h3>Busque a biografia do artista de sua escolha!</h3>
+                        <p>Insira o nome do artista no campo abaixo.</p>
+                        <form class="requires-validation" novalidate>
+
+                            <div class="col-md-12">
+                               <input class="form-control" type="text" name="name" placeholder="Nome do artista" value={this.state.value} onChange={this.handleChange} required/>
+                            </div>
+                            <div class="form-button mt-3">
+                              <button onClick={() => this.getArtistId()}>
+                                 Buscar Artista
+                              </button>
+                            </div>
+                        </form>
+                      
+                        <div>{this.state.artist_name}</div>
+                        <div>{this.state.biography}</div>
+                        
+                    </div>
+                </div>
+            </div>
         </div>
+    </div>
       );
     }
 }
